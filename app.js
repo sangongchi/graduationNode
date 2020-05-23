04da521aec8be90a52870ef2f380a46d3d13d90f
yp <yangpei3@100tal.com>
2020-05-23 23:03:31 +0800

    修改下载对应链接地址

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get('/', function (req, res) {
   res.send({ msg: 'you just sent a GET request, friend' });
 });
 app.get('/downloadUrl', (req, res) => {
-  let downloadUrl='http://192.168.3.129:3000/download?fileHref='+req.query.fileHref
+  let downloadUrl='http://sangongchi.top:3000/download?fileHref='+req.query.fileHref
   res.send({err:0,downloadUrl:downloadUrl})
 });
 app.get('/download', (req, res) => {
@@ -62,7 +62,7 @@ app.get('/download', (req, res) => {
     if (err) {
       res.send('文件下载失败');
     }else{
-      let downloadUrl='http://192.168.3.129:3000/download?fileHref='+fileHref
+      let downloadUrl='http://sangongchi.top:3000/download?fileHref='+fileHref
       console.log(downloadUrl)
     }
   });
